5044b64ca574253c631ce3b6301d811da5519cea
natalia <mellomartins.naty@gmail.com>
2021-09-26 21:56:56 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      search: value,
+    });
+  }
+
   render() {
+    const minLength = 2;
+    const { search } = this.state;
+
     return (
-      <div>
+      <div data-testid="page-search">
         <Header />
-        <h2 data-testid="page-search">Search</h2>
+        <h1>Search</h1>
         <form>
-          <input type="text" data-testid="search-artist-input" />
-          <button type="submit" data-testid="search-artist-button">Pesquisar</button>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ search }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ search.length < minLength }
+          >
+            Pesquisar
+          </button>
         </form>
       </div>
     );
